6bbc4c51f1172384257b0bca1731fcaceb69a50e
hashtagesteban <114188090+hashtagesteban@users.noreply.github.com>
2023-05-25 15:16:01 +0000

    added complete feature

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 todos = []
 
 while True:
-  user_action = input("Type add, show, edit or exit: ")
+  user_action = input("Type add, show, edit, complete or exit: ")
   user_action = user_action.strip()
 
   match user_action:
@@ -11,14 +11,20 @@ while True:
       todos.append(todo)
 
     case 'show':
-      for item in todos:
-        print(item)
+      for index, item in enumerate(todos):
+        row = f"{index + 1}-{item}"
+        print(row)
 
     case 'edit':
       number = int(input("Number of the todo to edit: "))
       number= number - 1
       new_todo = input('Enter new to do:')
       todos[number] = new_todo
+
+    case 'complete':
+      number = int(input("Number of the todo to complete: "))
+      number = number -1
+      todos.pop(number)
 
     case 'exit':
       break
