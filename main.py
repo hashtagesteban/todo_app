8b1c27bd05058a4b4cd88fa4229716b2bad6e861
hashtagesteban <114188090+hashtagesteban@users.noreply.github.com>
2023-05-25 15:53:41 +0000

    added functionality to show

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ while True:
       file.close()
 
     case 'show':
+      file = open('todos.txt', 'r')
+      todos = file.readlines()
+      file.close()
+      
       for index, item in enumerate(todos):
         row = f"{index + 1}-{item}"
         print(row)
