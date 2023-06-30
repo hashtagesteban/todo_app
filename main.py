cc87ce35d0d973b5be4b9d3960aa670538576f34
hashtagesteban <114188090+hashtagesteban@users.noreply.github.com>
2023-06-30 16:32:46 +0000

    added date module

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from functions import get_todos, write_todos
+import time
+import functions
 
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
@@ -7,15 +10,15 @@ while True:
     if user_action.startswith('add'):
       todo = user_action[4:]
 
-      todos = get_todos()
+      todos = functions.get_todos()
       
       todos.append(todo + '\n')
 
-      write_todos(todos)
+      functions.write_todos(todos)
 
     elif user_action.startswith('show'):
      
-     todos = get_todos()
+     todos = functions.get_todos()
         
      for index, item in enumerate(todos):
         item = item.strip('\n')
@@ -28,13 +31,13 @@ while True:
           print(number)
           number= number - 1
 
-          todos = get_todos()
+          todos = functions.get_todos()
                     
           new_todo = input('Enter new to do: ')
           todos[number] = new_todo + '\n'
 
 
-          write_todos(todos)
+          functions.write_todos(todos)
         
         except ValueError:
           print("Your command is not valid.")
@@ -44,14 +47,14 @@ while True:
         try:
           number = int(user_action[9: ])
           
-          todos = get_todos()
+          todos = functions.get_todos()
 
           index = number - 1
           todos_to_remove = todos[index].strip('\n')
 
           todos.pop(number - 1)
 
-          write_todos(todos)
+          functions.write_todos(todos)
 
           message = f"To do {todos_to_remove} was removed from the list"
           print(message)
